0575279d867170e1d00446d1a215d9104c9b6261
baikarrohit <baikarrohit1999@gmail.com>
2023-02-19 14:35:11 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,30 @@
+//***  Examine Document Object *****//
+
+//console.log(document.location);
+//console.log(document.URL);
+//console.log(document.title);
+//document.title="DOM";
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.all);
+//document.all[6].textContent ='Hello!';
+//console.log(document.forms[0]);
+//console.log(document.links);
+
+//*** Get ElememntById ***//
+
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+//console.log(headerTitle);
+
+// headerTitle.textContent = 'hello';
+// headerTitle.innerText = 'goodbye';
+// console.log(headerTitle.innerText);
+//headerTitle.innerHTML = '<h3>Hello Coders!</h3>'; 
+
+// header.style.borderBottom = 'solid 4px #000';
+
+var Title = document.getElementsByClassName('title');
+Title[0].style.fontWeight = 'bold';
+Title[0].style.color = 'green';
